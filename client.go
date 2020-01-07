f9273e84ce8800b505f64571551441d0ca64d4d2
Revil Wang <revilwang@gmail.com>
2020-01-07 14:27:50 +0800

    add fee in spot orders and trades

--- client.go
+++ client.go
@@ -38,7 +38,7 @@ var (
 	xmlCheck  = regexp.MustCompile("(?i:[application|text]/xml)")
 )
 
-// APIClient manages communication with the Gate API v4 API v4.9.0
+// APIClient manages communication with the Gate API v4 API
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
