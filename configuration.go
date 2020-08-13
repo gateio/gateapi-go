ac6b4390ec086f87a34e6169d1058ac3454704a8
Revil Wang <revilwang@gmail.com>
2020-08-13 16:35:39 +0800

    prepare 4.15.3 release

--- configuration.go
+++ configuration.go
@@ -90,7 +90,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "https://api.gateio.ws/api/v4",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/4.15.2/go",
+		UserAgent:     "OpenAPI-Generator/4.15.3/go",
 		Debug:         false,
 		Servers: []ServerConfiguration{
 			{
